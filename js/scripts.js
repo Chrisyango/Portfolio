562451090c50c20437fa901c92a25ccc2a3a7598
Chris Yang <cyang287@yahoo.com>
2020-12-09 04:00:11 -0500

    form testing

--- js/scripts.js
+++ js/scripts.js
@@ -46,18 +46,6 @@
     let liNumber = $(this).index();
     navContainer.find('.nav-item').eq(liNumber).css('display', 'block');
   });
-
-  // Contact Form
-  // let contact = $('#contact');
-  // let createForm = $('<form action="POST" data-netlify="true" id="contact-form">\
-  //   <h2>Contact Me</h2>\
-  //   <p><label>Name:</label> <input name="name" type="text" /></p>\
-  //   <p><label>Email Address:</label> <input name="email" type="email" /></p>\
-  //   <p><label>Message:</label>  <textarea name="message"></textarea> </p>\
-  //   <p><div data-netlify-recaptcha="true"></div></p>\
-  //   <p><button type="submit" value="Send" style="cursor: pointer;">Send</button></p>\
-  //   </form>');
-  // contact.append(createForm);
   
   // Validate Form
   const constraints = {
